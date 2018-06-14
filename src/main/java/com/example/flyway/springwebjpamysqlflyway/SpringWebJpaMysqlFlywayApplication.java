583ae8debdd59071fd9e8d4b59c01edfbf1f413a
Parker_z7 <Park.Promsawat@gmail.com>
2018-06-14 16:15:37 +0700

    Initial commit

---
+++ src/main/java/com/example/flyway/springwebjpamysqlflyway/SpringWebJpaMysqlFlywayApplication.java
@@ -0,0 +1,12 @@
+package com.example.flyway.springwebjpamysqlflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJpaMysqlFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebJpaMysqlFlywayApplication.class, args);
+	}
+}
